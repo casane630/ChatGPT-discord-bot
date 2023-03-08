2767a2cc35fb6d14d19c7bc21eba1b00cfb8f158
matatabi <aqua9n@gmail.com>
2023-03-08 15:41:00 +0900

    Create cgpt.py

---
+++ cgpt.py
@@ -0,0 +1,11 @@
+import openai
+import os
+from dotenv import load_dotenv
+load_dotenv()
+def cgpt(data):
+    openai.api_key = os.environ['cgptKey']
+    return openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=data)["choices"][0]["message"]["content"]
+messages=[
+    {"role": "system", "content": "あなたは会話に日本語で返答するアシスタントです。"},
+    {"role": "system", "content": "あなたは一つの題材についてだけ答えます"}
+    ]
