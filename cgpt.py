ebeac4c87ecb7b98503aa1d406eb99caaceb424d
matatabi <aqua9n@gmail.com>
2023-03-08 15:46:51 +0900

    Update cgpt.py

--- cgpt.py
+++ cgpt.py
@@ -7,5 +7,5 @@ def cgpt(data):
     return openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=data)["choices"][0]["message"]["content"]
 messages=[
     {"role": "system", "content": "あなたは会話に日本語で返答するアシスタントです。"},
-    {"role": "system", "content": "あなたは一つの題材についてだけ答えます"}
+    {"role": "system", "content": "あなたは一つの題材についてだけ答えます。"}
     ]
